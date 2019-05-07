12b4a22771e27fca609502e85b345589aaa59656
maxtillberg <max.tillberg@bengtdahlgren.se>
2019-05-07 10:17:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import dash
 import dash_table
 import pandas as pd
+import dash_core_components as dcc
 
 #df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/solar.csv')
 df = pd.read_csv('2011_us_ag_exports.csv')
@@ -8,6 +9,19 @@ df1 = df[1:5]
 app = dash.Dash(__name__)
 
 server = app.server
+
+
+
+dcc.Checklist(
+    options=[
+        {'label': 'New York City', 'value': 'NYC'},
+        {'label': 'Montréal', 'value': 'MTL'},
+        {'label': 'San Francisco', 'value': 'SF'}
+    ],
+    values=['MTL', 'SF']
+)
+
+
 
 app.layout = dash_table.DataTable(
     id='table',
