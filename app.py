73cd60395f7f057b206a1841099580edf8a69ce7
maxtillberg <max.tillberg@bengtdahlgren.se>
2019-05-07 13:31:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -42,11 +42,12 @@ app.layout = html.Div(children=[
     
     dcc.Dropdown(
         options=[
-            {'label': 'Unfiltered daylight', 'value': 'NYC'},
+            {'label': 'Unfiltered daylight', 'value': 'UFD'},
             {'label': u'Daylight through Parans 50m', 'value': 'MTL'},
             {'label': 'Daylight through 2-pane thermal glass', 'value': 'SF'}
+            {'label': 'Daylight through 2-pane solar control glass', 'value': 'SCG'}
         ],
-        value=['NYC', 'SF'],
+        value=['UFD', 'SF'],
         multi=True
     ),
     
