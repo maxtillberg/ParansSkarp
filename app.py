fed86c66a7a1a8213f67125f464b6abc60b27283
maxtillberg <max.tillberg@bengtdahlgren.se>
2019-05-07 10:44:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,49 +4,49 @@ import pandas as pd
 import dash_core_components as dcc
 import dash_html_components as html
 
-#df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/solar.csv')
-#df = pd.read_csv('2011_us_ag_exports.csv')
-#df1 = df[1:5]
-#app = dash.Dash(__name__)
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/solar.csv')
+df = pd.read_csv('2011_us_ag_exports.csv')
+df1 = df[1:5]
+app = dash.Dash(__name__)
 
-#server = app.server
-
-
-
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    )
-])
+server = app.server
 
 
 
 
-#app.layout = dash_table.DataTable(
-#    id='table',
-#    columns=[{"name": i, "id": i} for i in df1.columns],
-#    data=df1.to_dict("rows"),
-#)
+#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+#app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+#app.layout = html.Div(children=[
+#    html.H1(children='Hello Dash'),
+
+#    html.Div(children='''
+#        Dash: A web application framework for Python.
+#    '''),
+
+#    dcc.Graph(
+#        id='example-graph',
+#        figure={
+#            'data': [
+#                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+#                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+#            ],
+#            'layout': {
+#                'title': 'Dash Data Visualization'
+#            }
+#        }
+#    )
+#])
+
+
+
+
+app.layout = dash_table.DataTable(
+    id='table',
+    columns=[{"name": i, "id": i} for i in df1.columns],
+    data=df1.to_dict("rows"),
+)
 
 
 
