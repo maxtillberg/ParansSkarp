5136cb8ad7aca539bdf84a14d948715fd345c138
maxtillberg <max.tillberg@bengtdahlgren.se>
2019-05-17 13:18:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 import pandas as pd
-from dash.dependencies import Input
 
 ########### Get Data
 
