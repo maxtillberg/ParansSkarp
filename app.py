df7459398c80e0385ffdef80199c035abc41a927
maxtillberg <max.tillberg@bengtdahlgren.se>
2019-05-21 14:46:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ app.layout = html.Div(children=[
             {'label': 'Warm white LED', 'value': 'WLED'}
         ],
         value=['UFD'],
-        multi=True
+        multi=False
     ),
     
     html.Div(id='my-div'),
@@ -158,33 +158,31 @@ def update_graph(country_values):
     } 
  
  
- @app.callback(
-    dash.dependencies.Output('spektra', 'figure'),
-    [dash.dependencies.Input('droplista', 'value')])
-def update_graph(vald_data):
-    df_val = df_test[vald_data]
-    df_val.assign(Wavelength_nm=df_test.Wavelength_nm)
-
-    return {
-        'data': go.Scatter(
-            x=df_val.Wavelength_nm,
-            y=df_val.UFD,
-            mode='lines',
-            name="UDUDUD",
-            marker={
-                'size': 15,
-                'opacity': 0.5,
-                'line': {'width': 0.5, 'color': 'white'}
-            }
-        ) ,
-        'layout': go.Layout(
-            title="GDP over time, by country",
-            xaxis={'title': 'Wavelength [nm]'},
-            yaxis={'title': 'Y-axel'},
-            margin={'l': 60, 'b': 50, 't': 80, 'r': 0},
-            hovermode='closest'
-        )
-    } 
+# @app.callback(
+ #   dash.dependencies.Output('spektra', 'figure'),
+#    [dash.dependencies.Input('droplista', 'value')])
+#def update_graph(vald_data):
+#    df_val = df_test[vald_data]
+#    df_val.assign(Wavelength_nm=df_test.Wavelength_nm)
+#
+#    return {
+ #       'data': go.Scatter(
+ #           x=df_val.Wavelength_nm,
+ #           y=df_val.UFD,
+ #           mode='lines',
+ #           name="UDUDUD",
+ #           marker={
+ #               'size': 15,
+ #               'opacity': 0.5,
+ #               'line': {'width': 0.5, 'color': 'white'}
+ #           }
+#        ) ,
+ #       'layout': go.Layout(
+ #           title="GDP over time, by country",
+#            xaxis={'title': 'Wavelength [nm]'},
+#            yaxis={'title': 'Y-axel'}
+#        )
+#    } 
  
  
 ########### Run app!
